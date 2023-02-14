3a5b53ff958516974ac0e6f3fff19e85bae412e3
Guudz <Kilian1702@outlook.com>
2023-02-14 09:40:46 +0100

    Initial commit

---
+++ shared/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config = {
+    AllowedLicences = {
+        ["license:762fc4d3f0786dfc449d7920fa2ac89b4b84"] = true -- votre license
+    },
+    TypeMarker = {Index = 1},
+    HeightMarker = {Index = 1},
+    RotateMarker = {IndexX = 1, IndexY = 1, IndexZ = 1},
+    ListColor = {IndexMarker = 1, IndexRect = 1, "Rouge", "Vert", "Bleu", "Opacité"},
+    CoordsList = {Index = 1, "vector3", "x, y, z", "{x, y, z}"},
+    DrawRectPostion = {X = 0.5, Y = 0.5},
+    DrawRectWidth = 0.5,
+    DrawRectHeight = 0.5,
+}
